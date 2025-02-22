5726682cbbdd0671f6d8189aa00ce6a8d00b76fe
Ahmed-A807 <wafaa1311ahmed@gmail.com>
2025-02-22 21:09:50 +0200

    Wafaa Test.py

--- Test.py
+++ Test.py
@@ -3,3 +3,7 @@ name1 = "Maryam Eid"
 id_number1 = 2320603  
 print("الاسم:", name1)
 print("ID:", id_number1)
+name2="Wafaa Abd Elrouf"
+Id_number2=2320728
+Print ("الاسم: ", name2)
+Print ("ID: ", id_number2)
